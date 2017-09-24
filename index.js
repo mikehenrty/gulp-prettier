5c9c08b5d0cb57f6d391151a9f2556cf97a0b4ed
Michael Henretty <michael.henretty@gmail.com>
2017-09-24 13:57:16 +0200

    update prettier version, and remove default options

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const through = require('through2'),
   gutil = require('gulp-util'),
   prettier = require('prettier'),
-  merge = require('merge'),
   applySourceMap = require('vinyl-sourcemaps-apply');
 
 var PluginError = gutil.PluginError;
 
-module.exports = function(opt) {
+module.exports = function(options) {
   function transform(file, encoding, callback) {
     if (file.isNull())
       return callback(null, file);
@@ -18,24 +17,6 @@ module.exports = function(opt) {
 
     let data;
     let str = file.contents.toString('utf8');
-
-    options = merge(
-      {
-        // Fit code within this line limit
-        printWidth: 80,
-        // Number of spaces it should use per tab
-        tabWidth: 2,
-        // Use the flow parser instead of babylon
-        useFlowParser: true,
-        // If true, will use single instead of double quotes
-        singleQuote: false,
-        // Controls the printing of trailing commas wherever possible
-        trailingComma: false,
-        // Controls the printing of spaces inside array and objects
-        bracketSpacing: true
-      },
-      opt
-    );
 
     try {
       data = prettier.format(str, options);
